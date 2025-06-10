436cc82281ca00b86994c30490c7b7ecd4c462e5
Heitor Sousa Cruz <heitor.sousacruz@hotmail.com>
2025-06-09 22:32:13 -0300

    Aula21

---
+++ aula21/aula21.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!==diferente estrito (valor e tipo)
+*/
+
+console.log("ab" > "aba")
+console.log(1 == "1")
+console.log(1 === "1")
+console.log(1 != "1")
+console.log(1 !== "1")
